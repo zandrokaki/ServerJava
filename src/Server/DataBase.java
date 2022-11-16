bd9e45feece471cb532957bab347a96ea7afc6ed
Sandro <sandrogarcia97@hotmail.com>
2022-11-16 16:47:36 +0100

    Update 2.0

--- src/Server/DataBase.java
+++ src/Server/DataBase.java
@@ -1,6 +1,10 @@
 package Server;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Base64;
 import java.util.HashMap;
+import java.util.Random;
 
 public class DataBase {
     private HashMap<String, User> userMap;
@@ -26,8 +30,13 @@ public class DataBase {
         if(!isRegistered(name))
             throw new Exception("This user doesn't exist");
 
-        
-        return "";
+        Random rand = new Random();
+        int r1 = rand.nextInt(1000000);
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(Integer.toString(r1).getBytes(StandardCharsets.UTF_8));
+        String encoded = Base64.getEncoder().encodeToString(hash);
+
+        return encoded;
         
     }
 
--- src/Server/ThreadServer.java
+++ src/Server/ThreadServer.java
@@ -5,28 +5,45 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.HashMap;
-
 public class ThreadServer extends Thread {
-
     private Socket socket;
     private ArrayList<Socket> clients;
     private HashMap<Socket, String> clientNameList;
-
-    public ThreadServer(Socket socket, ArrayList<Socket> clients, HashMap<Socket, String> clientNameList) {
+    private DataBase db;
+    public ThreadServer(Socket socket, ArrayList<Socket> clients, HashMap<Socket, String> clientNameList, DataBase db) {
         this.socket = socket;
         this.clients = clients;
         this.clientNameList = clientNameList;
+        this.db = db;
     }
-
     @Override
     public void run() {
         try {
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
             while (true) {
                 String outputString = input.readLine();
                 if (outputString.equals("logout")) {
                     throw new SocketException();
+                }
+                if((outputString.contains("reg-114101103"))){
+                    String user = outputString.split("reg-114101103")[1].split("115101112097114097100111114")[0];
+                    String password = outputString.split("reg-114101103")[1].split("115101112097114097100111114")[1];
+                    try{
+                        db.registerUser(user, password);
+                        sendMessageToSender(socket, "reg-114101103"+"successfull");
+                    }catch(Exception e){
+                        sendMessageToSender(socket, "reg-114101103"+e.getMessage());
+                    }
+                }
+                if((outputString.contains("log-108111103"))){
+                    String user = outputString.split("log-108111103")[1].split("115101112097114097100111114")[0];
+                    String password = outputString.split("log-108111103")[1].split("115101112097114097100111114")[1];
+                    try{
+                        db.registerUser(user, password);
+                    }catch(Exception e){
+                    }
+                }
+                if((outputString.startsWith("tok-116111107"))){
                 }
                 if (!clientNameList.containsKey(socket)) {
                     String[] messageString = outputString.split(":", 2);
@@ -48,7 +65,23 @@ public class ThreadServer extends Thread {
             System.out.println(e.getStackTrace());
         }
     }
-
+    private void sendMessageToSender(Socket sender, String outputString){
+        Socket socket;
+        PrintWriter printWriter;
+        int i = 0;
+        while (i < clients.size()) {
+            socket = clients.get(i);
+            i++;
+            try {
+                if (socket == sender) {
+                    printWriter = new PrintWriter(socket.getOutputStream(), true);
+                    printWriter.println(outputString);
+                }
+            } catch (IOException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
     private void showMessageToAllClients(Socket sender, String outputString) {
         Socket socket;
         PrintWriter printWriter;
